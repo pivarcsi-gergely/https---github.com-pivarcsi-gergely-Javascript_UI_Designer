2f95c9643f2b25a5db832a2887ff28d8f55e396d
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-11-09 15:11:46 +0100

    betumeret pipa

--- main.js
+++ main.js
@@ -5,8 +5,14 @@ function szoveg() {
 
  }
 
+ function betumeret() {
+     let inputBetumeret = document.getElementById("inputBetumeret").value;
+     document.getElementById("elonezet").style.fontSize = inputBetumeret + "pt";
+ }
+
 function init() {
     document.getElementById("inputSzoveg").addEventListener("input", szoveg);
+    document.getElementById("inputBetumeret").addEventListener("input", betumeret);
 }
 
 document.addEventListener("DOMContentLoaded", init);

05ce6ad5d3696faed6fefeaea24050b732881f3a
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-11-09 15:22:01 +0100

    szoveg es hatterszin pipa

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 function szoveg() {
     let szoveg = document.getElementById("inputSzoveg").value;
- 
     document.getElementById("elonezet").innerHTML = szoveg;
 
  }
@@ -10,9 +9,21 @@ function szoveg() {
      document.getElementById("elonezet").style.fontSize = inputBetumeret + "pt";
  }
 
+ function szovegszin() {
+     let inputSzovegszin = document.getElementById("inputSzovegszin").value;
+     document.getElementById("elonezet").style.color = inputSzovegszin;
+ }
+
+ function hatterszin() {
+     let inputHatterszin = document.getElementById("inputHatterszin").value;
+     document.getElementById("elonezet").style.backgroundColor = inputHatterszin;
+ }
+
 function init() {
     document.getElementById("inputSzoveg").addEventListener("input", szoveg);
     document.getElementById("inputBetumeret").addEventListener("input", betumeret);
+    document.getElementById("inputSzovegszin").addEventListener("input", szovegszin);
+    document.getElementById("inputHatterszin").addEventListener("input", hatterszin);
 }
 
 document.addEventListener("DOMContentLoaded", init);
